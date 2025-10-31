ece8f2370d224695ac59b07936dfb55684c989f2
Gabe Dunn <gabe@gabedunn.dev>
2025-10-30 21:09:16 -0700

    feat: add types to safebrowsing lookup

--- src/safeBrowsing.ts
+++ src/safeBrowsing.ts
@@ -1,34 +1,94 @@
+// types from https://github.com/hckhanh/google-safe-browsing
+
+type ThreatType = 'MALWARE' | 'POTENTIALLY_HARMFUL_APPLICATION' | 'SOCIAL_ENGINEERING' | 'THREAT_TYPE_UNSPECIFIED' | 'UNWANTED_SOFTWARE'
+
+type PlatformType =
+	| 'ALL_PLATFORMS'
+	| 'ANDROID'
+	| 'ANY_PLATFORM'
+	| 'CHROME'
+	| 'IOS'
+	| 'LINUX'
+	| 'OSX'
+	| 'PLATFORM_TYPE_UNSPECIFIED'
+	| 'WINDOWS'
+
+type ThreatEntryType = 'EXECUTABLE' | 'THREAT_ENTRY_TYPE_UNSPECIFIED' | 'URL'
+
+type ThreatEntry = { hash: string } | { url: string } | { digest: string }
+
+interface ClientInfo {
+	clientId: string
+	clientVersion: string
+}
+
+interface ThreatInfo {
+	threatTypes: ThreatType[]
+	platformTypes: PlatformType[]
+	threatEntryTypes: ThreatEntryType[]
+	threatEntries: ThreatEntry[]
+}
+
+interface MetadataEntry {
+	key: string
+	value: string
+}
+
+interface ThreatEntryMetadata {
+	entries: MetadataEntry[]
+}
+
+interface ThreatMatch {
+	threatType: ThreatType
+	platformType: PlatformType
+	threatEntryType: ThreatEntryType
+	threat: ThreatEntry
+	threatEntryMetadata: ThreatEntryMetadata
+	cacheDuration: string
+}
+
+interface ThreatMatch {
+	threatType: ThreatType
+	platformType: PlatformType
+	threatEntryType: ThreatEntryType
+	threat: ThreatEntry
+	threatEntryMetadata: ThreatEntryMetadata
+	cacheDuration: string
+}
+
+interface FindThreatMatchesRequest {
+	client: ClientInfo
+	threatInfo: ThreatInfo
+}
+
+interface FindThreatMatchesResponse {
+	matches?: ThreatMatch[]
+}
+
 export const checkSafeBrowsing = async (env: Env, dest: string): Promise<boolean> => {
 	const key = env.GOOGLE_SAFE_BROWSING_KEY
 
 	try {
-		const safeTest = await fetch(
-			`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`,
-			{
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({
-					client: {
-						clientId: 'shortaf',
-						clientVersion: '1.0.0',
-					},
-					threatInfo: {
-						threatTypes: [
-							'MALWARE',
-							'POTENTIALLY_HARMFUL_APPLICATION',
-							'SOCIAL_ENGINEERING',
-							'UNWANTED_SOFTWARE',
-							'THREAT_TYPE_UNSPECIFIED'
-						],
-						platformTypes: ['ANY_PLATFORM'],
-						threatEntryTypes: ['URL'],
-						threatEntries: [{ url: dest }],
-					},
-				}),
+		const body: FindThreatMatchesRequest = {
+			client: {
+				clientId: 'shortaf',
+				clientVersion: '1.0.0',
 			},
-		)
+			threatInfo: {
+				threatTypes: ['MALWARE', 'POTENTIALLY_HARMFUL_APPLICATION', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE', 'THREAT_TYPE_UNSPECIFIED'],
+				platformTypes: ['ANY_PLATFORM'],
+				threatEntryTypes: ['URL'],
+				threatEntries: [{ url: dest }],
+			},
+		}
 
-		const result = await safeTest.json()
+		const safeTest = await fetch(`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${key}`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(body),
+		})
+
+		const result = (await safeTest.json()) as FindThreatMatchesResponse
 
 		// if there aren't any matches, the url is safe
 		if (!result.matches) {
